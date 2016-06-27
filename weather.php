abc90974f192e1034121553e5bbfa1366e27c2b0
Kiriu Tomoaki <tk9510159@gmail.com>
2016-06-27 10:39:07 +0900

    add weather.php

---
+++ weather.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>天気予報</title>
+<meta charset="utf-8">
+</head>
+<body>
+<a href="http://weather.livedoor.com/forecast/rss/primary_area.xml">地域とIDの定義表</a>
+<form method="GET" action="./weather.php">
+<input type="text" name="cityCode">
+<input type="submit" value="検索" style="display:inline">
+</form>
+<hr>
+<?php
+if(isset($_GET["cityCode"])){
+	$code = $_GET["cityCode"];
+	$url = 'http://weather.livedoor.com/forecast/webservice/json/v1';
+	$query = ['city' => $code];
+	$proxy = array("http" => array("proxy" => "tcp://proxy.kmt.neec.ac.jp:8080", "request_fulluri" => true));
+			
+	$proxy_context = stream_context_create($proxy);
+    $response = file_get_contents(
+	    $url . '?' . http_build_query($query),
+        false,
+		$proxy_context);
+
+    $result = json_decode($response, true);
+
+    print("<p>" . $result["location"]["city"] . "</p>");
+    foreach($result["forecasts"] as $forecast){
+    	print("<p>" . $forecast["date"] . "<img style='display:inline;' src='" . $forecast["image"]["url"] . "'>" . $forecast["telop"] . "</p>");
+    }
+}
+?>
+</body>
+</html>
